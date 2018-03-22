084383c741bf1b9cd14add77d41fa4bf0837d9c7
naresh ganji <naresh25ganji@gmail.com>
2018-03-21 22:14:53 -0400

    update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,31 +4,33 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'white',
       marginBottom: '1.45rem',
-    }} 
+    }}
   >
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
+        maxWidth: '75%',
         padding: '1.45rem 1.0875rem',
+        borderBottom: '2px solid #ccc',
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/profile"
           style={{
-            color: 'white',
+            color: 'LightSkyBlue',
             textDecoration: 'none',
+            fontSize: 'x-large',
           }}
         >
           Naresh Ganji
     </Link>
         <ul style={{ listStyle: 'none', float: 'right' }}>
-          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'white', textDecoration: 'none', fontSize: 'x-large' }} to="/profile/">Profile</Link></li>
-          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'white', textDecoration: 'none', fontSize: 'x-large' }} to="/projects/">Projects</Link></li>
-          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'white', textDecoration: 'none', fontSize: 'x-large' }} to="/blog/">Blog</Link></li>
+          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'grey', textDecoration: 'none', fontSize: 'medium' }} to="/profile/">Profile</Link></li>
+          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'black', textDecoration: 'none', fontSize: 'medium' }} to="/projects/">Projects</Link></li>
+          <li style={{ display: 'inline-block', marginRight: '1rem' }}><Link style={{ color: 'black', textDecoration: 'none', fontSize: 'medium' }} to="/blog/">Blog</Link></li>
         </ul>
       </h1>
     </div >
