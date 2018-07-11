c3cd5247fa570c2406e971b1364223c552565271
naresh ganji <naresh25ganji@gmail.com>
2018-07-10 22:13:04 -0400

    add aphrodite support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { css } from 'aphrodite';
+import { styles } from './indexStyle';
 
 const Home = () => (
-  <div>
-    <h1>Hi people</h1>
+  <div >
+    <div className={css(styles.header)}>Full Stack Developer with 4+ years of experience in delivering high quality code to
+      production at scale. An enthusiastic team player who is passionate about solving challenging
+       problems that have a huge impact on customers.
+       </div>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+
     test page
   </div>
 )
